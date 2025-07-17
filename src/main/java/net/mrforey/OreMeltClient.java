327b0249535428deca6a11d5d1a49de64460e524
MrForey <semenov.stepan.1985@gmail.com>
2025-07-17 18:26:35 +0600

    initial

---
+++ src/main/java/net/mrforey/OreMeltClient.java
@@ -0,0 +1,10 @@
+package net.mrforey;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class OreMeltClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
